e0703c63354c377fe5ea00d04d948b49f7803815
veszg <vesz.gergo@gmail.com>
2026-02-18 01:07:04 +0100

    Modified app to manage any number of packages fetched from the database

--- app/src/main/java/com/remoteupdatemanager/ui/UpdateActivity.java
+++ app/src/main/java/com/remoteupdatemanager/ui/UpdateActivity.java
@@ -1,8 +1,11 @@
 package com.remoteupdatemanager.ui;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
@@ -12,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -21,29 +25,40 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import com.remoteupdatemanager.api.PraxCloud;
+import com.remoteupdatemanager.api.entity.ApkPublicDescription;
 import com.remoteupdatemanager.install.PraxPackageInstaller;
 import com.remoteupdatemanager.R;
 import com.remoteupdatemanager.download.ApkDownloader;
 import com.remoteupdatemanager.download.CallbackByteChannel;
 import com.remoteupdatemanager.download.ProgressCallback;
 
+import static com.remoteupdatemanager.constants.PraxConstants.Api.PRAXCLOUD_API_URL;
+import static com.remoteupdatemanager.constants.PraxConstants.ApkUpdate.EVENT_INSTALL_COMPLETE;
+
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Stack;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class UpdateActivity extends AppCompatActivity {
     private final static String TAG = UpdateActivity.class.getSimpleName();
-
-    private static final String EXTRA_APK_FILENAME = "com.praxupdatemanager.APK_FILENAME";
-    private static final String EXTRA_UPDATE_VERSION = "com.praxupdatemanager.APK_UPDATE_VERSION";
-    private static final String EXTRA_CURRENT_VERSION = "com.praxupdatemanager.APK_CURRENT_VERSION";
-    private static final String ACTION_PRAX_REMOTE_UPDATE = "com.praxupdatemanager.ACTION_PRAX_REMOTE_UPDATE";
     // ~200MB
     private static final long APK_EXPECTED_FILE_SIZE = 200 * 1024 * 1024L; // TODO put on cloudserver as metadata
+    private static final int MIN_WAIT_TIME_LOADING_MS = 1500;
 
     public static final String PRAXTOUR_APP_PACKAGE_NAME = "com.videostreamtest";
+    private String accountToken;
+    Stack<ApkPublicDescription> packagesInfo = new Stack<>();
 
     private TextView titleTextView;
+    private ProgressBar checkingForUpdatesLoadingWheel;
     private TextView descriptionTextView;
     private TextView statusTextView;
     private Button startInstallationButton;
@@ -67,15 +82,12 @@ public class UpdateActivity extends AppCompatActivity {
                 public void onActivityResult(ActivityResult result) {
                     switch (result.getResultCode()) {
                         case Activity.RESULT_OK:
-                            Log.d(TAG, "Greg1");
-                            runOnUiThread(() -> showInstallationStep());
+                            runOnUiThread(UpdateActivity.this::showInstallationStep);
                             break;
                         case Activity.RESULT_CANCELED:
-                            Log.d(TAG, "Greg2");
                             // TODO show permission required page
                             break;
                         default:
-                            Log.d(TAG, "Greg3");
                             // TODO error occurred during permission giving, contact service@praxtour.com
                             break;
                     }
@@ -89,6 +101,7 @@ public class UpdateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_update);
 
         titleTextView = findViewById(R.id.title_textview);
+        checkingForUpdatesLoadingWheel = findViewById(R.id.checking_for_updates_loading_wheel);
         descriptionTextView = findViewById(R.id.description_textview);
         statusTextView = findViewById(R.id.status_textview);
         startInstallationButton = findViewById(R.id.start_installation_button);
@@ -102,53 +115,108 @@ public class UpdateActivity extends AppCompatActivity {
 
         appAccountInfoTextView.setText(String.format("Version %s", getAppVersion()));
 
-        openPraxtourButton.setOnClickListener(view -> launchPraxtourMainApp(this));
+        openPraxtourButton.setOnClickListener(view -> launchPraxtourMainApp());
         restartProcessButton.setOnClickListener(view -> restartUpdateProcess());
 
-        Intent updateIntent = getIntent();
-        if (!ACTION_PRAX_REMOTE_UPDATE.equals(updateIntent.getAction())) {
-            downloadStatusProgressBar.setVisibility(View.GONE);
-            titleTextView.setText(getString(R.string.alternative_title));
-            descriptionTextView.setText(getString(R.string.alternative_description));
-            statusTextView.setVisibility(View.GONE);
-            openPraxtourButton.setVisibility(View.VISIBLE);
-            openPraxtourButton.requestFocus();
-            return;
-        }
-
-        String updateFilename = updateIntent.getStringExtra(EXTRA_APK_FILENAME);
-        String currentApkVersion = updateIntent.getStringExtra(EXTRA_CURRENT_VERSION);
-        String newApkVersion = updateIntent.getStringExtra(EXTRA_UPDATE_VERSION);
-
-        descriptionTextView.setText(String.format(getString(R.string.app_update_description), currentApkVersion, newApkVersion));
-        statusTextView.setText(getString(R.string.app_update_status_download_text));
+        accountToken = "redacted";
+//        accountToken = getIntent().getStringExtra(EXTRA_ACCOUNT_TOKEN);
 
         new Thread(() -> {
-            File apkFile = ApkDownloader.download(this, updateFilename, APK_EXPECTED_FILE_SIZE, new ProgressCallback() {
-                @Override
-                public void callback(CallbackByteChannel rbc, double progress) {
-                    downloadStatusProgressBar.setProgress((int) progress);
-                }
-            });
-            runOnUiThread(() -> downloadStatusProgressBar.setVisibility(View.GONE));
-
-            if (apkFile == null) {
-                displayErrorEncountered(ErrorStep.DOWNLOAD);
-                return;
+            try {
+                Thread.sleep(MIN_WAIT_TIME_LOADING_MS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Unexpected interrupt", e);
             }
-            Log.d(TAG, "Greg file is ok: " + apkFile.getAbsolutePath());
+            packagesInfo.addAll(fetchAllPackagePublicInfo());
+            runOnUiThread(() -> checkingForUpdatesLoadingWheel.setVisibility(View.GONE));
+            processNextPackage();
+        }).start();
+    }
 
-            startInstallationButton.setOnClickListener(view -> {
-                try {
-                    startInstallationButton.setVisibility(View.GONE);
-                    installationInProgressLayout.setVisibility(View.VISIBLE);
-                    boolean result = PraxPackageInstaller.installApk(this, apkFile);
-                    if (!result) throw new IOException("Unknown error:(");
-                } catch (IOException e) {
-                    displayErrorEncountered(ErrorStep.INSTALL);
-                    Log.e(TAG, "Greg uh-oh " + e);
+    private void processNextPackage() {
+        new Thread(() -> {
+            if (!packagesInfo.isEmpty()) {
+                ApkPublicDescription packageInfo = packagesInfo.pop();
+                String packageName = packageInfo.getPackageName();
+                if (packageName.equals(getPackageName()) || !packageShouldBeProcessed(packageInfo)) {
+                    processNextPackage();
+                    return;
                 }
-            });
+
+                runOnUiThread(this::showDownloadStep);
+                boolean downloaded = downloadApk(packageName);
+                if (!downloaded) return;
+
+                runOnUiThread(checkInstallPackagePermissionRunnable);
+                runOnUiThread(this::showInstallationStep);
+            } else {
+                launchPraxtourMainApp();
+            }
+        }).start();
+    }
+
+    private boolean packageShouldBeProcessed(ApkPublicDescription packageInfo) {
+        PackageInfo onDevicePackageInfo;
+
+        try {
+            onDevicePackageInfo = getPackageManager().getPackageInfo(packageInfo.getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            return true;
+        }
+
+        String onDevicePackageVersion = onDevicePackageInfo.versionName;
+        String remotePackageVersion = packageInfo.getVersion();
+
+        return isUpdateAvailable(onDevicePackageVersion, remotePackageVersion);
+    }
+
+    private boolean isUpdateAvailable(String installedVersion, String remoteVersion) {
+        String[] installedVersionSplit = installedVersion.split("\\.");
+        String[] remoteVersionSplit = remoteVersion.split("\\.");
+
+        if (installedVersionSplit.length != remoteVersionSplit.length) {
+            throw new RuntimeException("Version number patterns don't match!" +
+                    "\n\tLocal: " + installedVersion +
+                    "\n\tRemote: " + remoteVersion);
+        }
+
+        for (int i = 0; i < installedVersionSplit.length; i++) {
+            int local = Integer.parseInt(installedVersionSplit[i]);
+            int remote = Integer.parseInt(remoteVersionSplit[i]);
+
+            if (remote > local) return true;
+        }
+
+        return false;
+    }
+
+    private boolean downloadApk(String packageName) {
+        URL updateFileUrl = fetchUpdateLink(packageName);
+        File apkFile = ApkDownloader.download(this, updateFileUrl, APK_EXPECTED_FILE_SIZE, new ProgressCallback() {
+            @Override
+            public void callback(CallbackByteChannel rbc, double progress) {
+                downloadStatusProgressBar.setProgress((int) progress);
+            }
+        });
+        runOnUiThread(() -> downloadStatusProgressBar.setVisibility(View.GONE));
+
+        if (apkFile == null) {
+            runOnUiThread(() -> displayErrorEncountered(ErrorStep.DOWNLOAD));
+            return false;
+        }
+        Log.d(TAG, "Greg file is ok: " + apkFile.getAbsolutePath());
+
+        startInstallationButton.setOnClickListener(view -> {
+            try {
+                startInstallationButton.setVisibility(View.GONE);
+                installationInProgressLayout.setVisibility(View.VISIBLE);
+                boolean result = PraxPackageInstaller.installApk(this, apkFile);
+                if (!result) throw new IOException("Unknown error:(");
+            } catch (IOException e) {
+                runOnUiThread(() -> displayErrorEncountered(ErrorStep.INSTALL));
+                Log.e(TAG, "Greg uh-oh " + e);
+            }
+        });
 
         return true;
     }
@@ -202,21 +270,24 @@ public class UpdateActivity extends AppCompatActivity {
                         runOnUiThread(this);
                     });
                 });
-
-                return;
             }
-
-            showInstallationStep();
         }
     };
+
+    private void showDownloadStep() {
+        descriptionTextView.setText(getString(R.string.app_update_status_download_text));
+        descriptionTextView.setVisibility(View.VISIBLE);
+        permissionButton.setVisibility(View.GONE);
+        downloadStatusProgressBar.setVisibility(View.VISIBLE);
+    }
 
     private void showInstallationStep() {
         descriptionTextView.setVisibility(View.GONE);
         permissionButton.setVisibility(View.GONE);
         statusTextView.setText(getString(R.string.app_update_status_install_text));
+        statusTextView.setVisibility(View.VISIBLE);
         startInstallationButton.setVisibility(View.VISIBLE);
         startInstallationButton.requestFocus();
-        statusTextView.setVisibility(View.VISIBLE);
     }
 
     private void showPermissionRequiredPage() {
@@ -236,13 +307,13 @@ public class UpdateActivity extends AppCompatActivity {
         activityResultLauncher.launch(settingsIntent);
     }
 
-    public static void launchPraxtourMainApp(Context context) {
-        Intent launch = context.getPackageManager().getLaunchIntentForPackage(PRAXTOUR_APP_PACKAGE_NAME);
+    private void launchPraxtourMainApp() {
+        Intent launch = getPackageManager().getLaunchIntentForPackage(PRAXTOUR_APP_PACKAGE_NAME);
         if (launch != null) {
             launch.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
                     | Intent.FLAG_ACTIVITY_CLEAR_TOP
                     | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-            context.startActivity(launch);
+            startActivity(launch);
         }
     }
 
@@ -282,5 +353,17 @@ public class UpdateActivity extends AppCompatActivity {
         } catch (PackageManager.NameNotFoundException e) {
             return "ERROR";
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(installResultLocalReceiver, new IntentFilter(EVENT_INSTALL_COMPLETE));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(installResultLocalReceiver);
     }
 }
